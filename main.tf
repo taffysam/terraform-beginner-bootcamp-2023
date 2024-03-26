be3bea12166ff03cadb05be845c649c3e58df3c7
taffysam <tdzomba@gmail.com>
2024-03-26 13:48:22 +0000

    11 Creating random S3 bucket

--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@ terraform {
         source = "hashicorp/random"
         version = "3.5.1"
     }
+
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
 
@@ -13,8 +18,17 @@ terraform {
 resource "random_string" "bucket_name" {
   length           = 16
   special          = false
+  lower = true
+  upper = false
+  
  # override_special = ""
 }
+
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
+
+ }
 
 output "random_bucket_id" {
     value = random_string.bucket_name.id
