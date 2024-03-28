2e5c98b693ce5ea2c30e9b8e2e2ed31cd75fc16d
taffysam <tdzomba@gmail.com>
2024-03-28 02:18:43 +0000

    Content Versioning

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ user_uuid = var.user_uuid
 bucket_name = var.bucket_name
 error_html_filepath = var.error_html_filepath
 index_html_filepath = var.index_html_filepath
+content_version = var.content_version
 }
 
 data "aws_caller_identity" "current" {}
